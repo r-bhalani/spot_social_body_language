b182ce94071f3754b9373135a545d85dab7384b5
Alekhya Kuchimanchi <a1.kuchimanchi@gmail.com>
2021-10-20 18:20:09 -0500

    Demo code with functions for each movement
    Changes from the for loop with char array to calling each method for each function.
    *note* needs sleep in between each method for it to execute correctly
    when moving with the velocity needs a sleep after it stands.

--- choreography/demo.cpp
+++ choreography/demo.cpp
@@ -1,4 +1,3 @@
-
 #include <spot_teleop.h>
 
 #include <iostream>
@@ -15,8 +14,23 @@
 #include <sys/ioctl.h> //For FIONREAD.
 #include <termios.h>
 
+#include <spot/robot_layer.h>
+#include <spot/spot.h>
+
 //#include <spot/clients/robot_state.h>
-using namespace std;
+//using namespace std;
+
+double posX = 0;
+double posY = 0;
+double posZ = 0;
+double pitch = 0;
+double roll = 0;
+double yaw = 0;
+double velX = 0;
+double velY = 0;
+double rot = 0;
+Trajectory3D trajPose;
+Spot spot;
 
 //Call this at program start to setup for kbhit.
 void initTerminalInput()
@@ -40,182 +54,122 @@ int kbhit()
 	return nbbytes;
 }
 
-static string getWCharClean()
+static wchar_t getWCharClean()
 {
-   for (string line; getline(cin, line);) {
-        cout << line << endl;
-		return line;
+  static wchar_t inputWChar;
+  do
+  {
+    //Wait until there's an input character.
+    while (!kbhit())
+    {
     }
-
-	return NULL;
+    inputWChar = getwchar();
+	//Erase the valid character.
+	std::wcout << L"\b \b";
+    break;
+  } while (true);
+  return inputWChar;
 }
 
-bool run_motions(char[] commands) {
-	bool keepRunning = true;
-	while(1) {
-		for(int i = 0; i < sizeof(commands)/sizeof(commands[0]); i++) {
-			
-			// initialize velocities and angular velocity (rot)
-			double velX = 0;
-			double velY = 0;
-			double rot = 0;
+void walkInCircle() {
+	velX += 2.0;
+	rot += 1.5;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+}
 
-			// xy translation
-			int flag = 0;
+void sit() {
+	pitch -= 3.14/7;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+}
 
-			if (commands[i] == 'w') {
-				velX += 1.0;
-				flag = 1;
-			}
-			if (commands[i] == 'a') {
-				velY += 1.0;
-				flag = 1;
-			}
-			if (commands[i] == 's') {
-				velX -= 1.0;
-				flag = 1;
-			}
-			if (commands[i] == 'd') {
-				velY -= 1.0;
-				flag = 1;
-			}
-			if (commands[i] == 'q') {
-				rot += 0.5;
-				flag = 1;
-			}
-			if (commands[i]== 'e') {
-				rot -= 0.5;
-				flag = 1;
-			}
-
-			// orientation (once we figure out)
-			if (commands[i]== 'i') {
-				pitch += 3.14/14;
-				flag = 0;
-			}
-			if (commands[i]== 'j') {
-				roll += 3.14/16;
-				flag = 0;
-			}
-			if (commands[i] == 'k') {
-				pitch -= 3.14/14;
-				flag = 0;
-			}
-			if (commands[i] == 'l') {
-				roll -= 3.14/16;
-				flag = 0;
-			}
-			if (commands[i]== 'u') {
-				yaw -= 3.14/16;
-				flag = 0;
-			}
-			if (commands[i]== 'o') {
-				yaw += 3.14/16;
-				flag = 0;
-			}
-
-			// height / positions ?
-			if (commands[i] == 'r') {
-				posX += 0.2;
-				flag = 0;
-			}
-			if (commands[i] == 'f') {
-				posX -= 0.2;
-				flag = 0;
-			}
-			if (commands[i] =='t') {
-				posY += 0.2;
-				flag = 0;
-			}
-			if (commands[i] =='g') {
-				posY -= 0.2;
-				flag = 0;
-			}
-			if (commands[i]== 'y') {
-				posZ += 0.2;
-				flag = 0;
-			}
-			if (commands[i]== 'h') {
-				posZ -= 0.2;
-				flag = 0;
-			}
-			
-			
-			// keys with multiple moves at once
-			if (commands[i]== 'm') {
-				yaw += 3.14/8;
-				roll -= 3.14/16;
-				flag = 0;
-				std::wcout << "combining O and L" << std::endl;
-			}
-			if (commands[i] == 'n') {
-				yaw -= 3.14/8;
-				roll += 3.14/16;
-				flag = 0;
-				std::wcout << "combining U and J" << std::endl;
-			}
-			if (commands[i] == 'c') {
-				velX += 2.0;
-				flag = 1;
-				rot += 1.5;
-			}
-			
-
-			// exit
-			if (commands[i] == 'b') {
-				keepRunning = false;
-				// robot.move(sit);
-				spot.sit();
-				std::wcout << "TURNING OFF. PLEASE STANDBY." << std::endl;
-				roll = 0;
-				pitch = 0;
-				yaw = 0;
-				posX = 0;
-				posY = 0;
-				posZ = 0;
-				return false;
-			}
-			if (commands[i] == 'v') {
-				//keepRunning = false;
-				// robot.move(sit);
-				std::wcout << "RESETTING JOINT POSITIONS" << std::endl;
-				roll = 0;
-				pitch = 0;
-				yaw = 0;
-				posX = 0;
-				posY = 0;
-				posZ = 0;
-				
-			}
-			std::wcout << "KEYPRESS: " << commands[i] << std::endl;
-			Trajectory3D trajPose;
-			trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
-			spot.setBodyPose(trajPose, true);
-			
-			//printing position changes
-			std::wcout << "Positions" << std::endl;
-			std::wcout << "velX: " << velX << std::endl;
-			std::wcout << "velY: " << velY << std::endl;
-			std::wcout << "rot: " << rot << std::endl;
-			std::wcout << "roll: " << roll << std::endl;
-			std::wcout << "pitch: " << pitch << std::endl;
-			std::wcout << "yaw: " << yaw << std::endl;
-			std::wcout << "---------------" << std::endl;
-
-			// issue move
-			if (velY == 0 && velX == 0 && rot == 0 /*&& flag*/){
-				spot.stand();
-				
-			}
-			else {
-				spot.velocityMove(velX, velY, rot, 500, FLAT_BODY);
-				
-			}
-
-			sleep(1);
-		}
+void issueMove() {
+	// issue move
+	if (velY == 0 && velX == 0 && rot == 0 ){
+		spot.stand();
+		
 	}
+	else {
+		spot.velocityMove(velX, velY, rot, 500, FLAT_BODY);
+		
+	}
+	sleep(1);
 }
+
+void wagTail() {
+	//m 
+	yaw += 3.14/8;
+	roll -= 3.14/16;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+	issueMove();
+	sleep(1);
+	// 2*n
+	yaw -= 3.14/4;
+	roll += 3.14/8;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+	issueMove();
+	sleep(1);
+	//m 
+	yaw += 3.14/8;
+	roll -= 3.14/16;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+}
+
+void oneWag() {
+	yaw += 3.14/8;
+	roll -= 3.14/16;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+}
+
+void otherWag() {
+	yaw -= 3.14/4;
+	roll += 3.14/8;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+}
+
+void playBow() {
+	pitch += (3.14*3)/14;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+}
+
+void playBowWagTail() {
+	pitch += 3.14/7;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+	issueMove();
+	sleep(1);
+	wagTail();
+}
+
+void yawChange() {
+	yaw -= 3.14/16;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+}
+void rollChange() {
+	roll += 3.14/16;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+}
+void reset() {
+	pitch = 0;
+	posX = 0;
+	posY = 0;
+	posZ = 0;
+	pitch = 0;
+	roll = 0;
+	yaw = 0;
+	trajPose.addPointRPY(posX, posY, posZ, roll, pitch, yaw, 1);
+	spot.setBodyPose(trajPose, true);
+}
+
 
 // main function for running Spot clients
 int main(int argc, char *argv[]) {
@@ -226,35 +180,41 @@ int main(int argc, char *argv[]) {
 	const std::string password = argv[2];
 
 	// spotbase testing code
-	Spot spot;
 	spot.basicInit(username, password);
-	spot.stand();
-     
-	// move
-	initTerminalInput();
-	
-	
-	double posX = 0;
-	double posY = 0;
-	double posZ = 0;
-	double pitch = 0;
-	double roll = 0;
-	double yaw = 0;
-	bool run = true;
-	while(run) {
-		string input = getWCharClean();
-		if(input == "circles")
-			char commands[] = "ccccccccc";
-		//more if statements to determine what the user wants to run
-		run = run_command(commands);
-		
-	}
-	
-	
+	//ClientLayer::RobotStateClient data(username, password); 
 
-	//put this into an infinite while loop
+	// Trajectory2D trajTest;
+	// trajTest.addPoint(0.5, 0, 0, 2);
+	// //trajTest.addPoint(1, 0, 0, 2);
+	// robot.trajectoryMove(trajTest, ODOM, 10);
+	// sleep(10);
+	spot.stand();
+     sleep(1);
+	// move
+	//initTerminalInput();
+	bool keepRunning = true;
 	
-	
+	while(keepRunning) {
+		walkInCircle();
+		issueMove();
+		sleep(1);
+		walkInCircle();
+		issueMove();
+		sleep(1);
+		walkInCircle();
+		issueMove();
+		sleep(1);
+		 //printing position changes
+		 std::wcout << "Positions" << std::endl;
+		 std::wcout << "velX: " << velX << std::endl;
+		 std::wcout << "velY: " << velY << std::endl;
+		 std::wcout << "rot: " << rot << std::endl;
+		 std::wcout << "roll: " << roll << std::endl;
+		 std::wcout << "pitch: " << pitch << std::endl;
+		 std::wcout << "yaw: " << yaw << std::endl;
+		 std::wcout << "---------------" << std::endl;
+
+	keepRunning = false;	
+	}
 	return 0;
 }
-
